f24f01e1cf89e20a45d33637fdb81a559c2235fd
Andy Likuski <andy@likuski.org>
2018-09-11 15:07:47 +0200

    Version update

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ class CleanCommand(setuptools.Command):
 
 setuptools.setup(
     name="rescape_python_helpers",
-    version="0.0.3",
+    version="0.0.4",
     author="Andy Likuski",
     author_email="andy@likuski.org",
     description="Functional and geospatial helpers for Rescape projects",
