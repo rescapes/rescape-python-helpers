a59d7037b007d35d191e3ccb38844cb86b4ad00e
Andy Likuski <andy@likuski.org>
2023-02-27 12:39:25 +0100

    Version update

--- setup.py
+++ setup.py
@@ -21,10 +21,10 @@ class CleanCommand(setuptools.Command):
 
 setuptools.setup(
     name="rescape_python_helpers",
-    version="0.0.62",
+    version="0.0.63",
     author="Andy Likuski",
     author_email="andy@likuski.org",
-    description="Functional and geospatial helpers for Rescape projects",
+    description="Functional and geospatial helpers for Rescape projects. Pyramda library by from https://github.com/jackfirth/pyramda incorporated in after it stopped working with new Python relases",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/calocan/rescape-python-helpers",
